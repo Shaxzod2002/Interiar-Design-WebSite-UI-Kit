91de28aa7fbf17bc9700c92e13fa9751c8aa9ade
Shaxzodbek Raximov <raximovshaxzodbek2002@gmail.com>
2022-10-29 14:37:06 +0500

    seconde

--- src/components/Home/navbar.js
+++ src/components/Home/navbar.js
@@ -2,21 +2,23 @@ import logo from "../../images/home/nav/logo.png";
 
 export default function Navbar() {
   const navbar = document.createElement("div");
-  navbar.className = "flex w-[90%] mx-auto";
+  navbar.className = "navbar flex w-[90%] mx-auto";
   navbar.innerHTML = `
-       <div class="w-[40%] min-h-[80px] flex justify-start items-center">
+       <div class="nav-logo w-[40%] min-h-[80px] flex justify-start items-center">
         <img src="${logo}" alt="navbar logo" />
        </div>
-       <div class="w-[50%] min-h-[80px] flex justify-evenly items-center">
+       <div class="nav-menu transition-all w-[50%] min-h-[80px] flex justify-evenly items-center">
         <a href="#" class="w-[100px] text-center font-normal transition-all duration-500 hover:font-[600]">Home</a>
         <a href="#" class="w-[100px] text-center font-normal transition-all duration-500 hover:font-[600]">About Us</a>
         <a href="#" class="w-[100px] text-center font-normal transition-all duration-500 hover:font-[600]">Services</a>
         <a href="#" class="w-[100px] text-center font-normal transition-all duration-500 hover:font-[600]">Our Teams</a>
        </div>
-       <div class="w-[10%] min-h-[80px] flex justify-center items-center">
+       <div class="nav-login w-[10%] min-h-[80px] flex justify-end items-center">
         <a href="#" class="w-[120px] border border-white py-[8px] px-3 bg-blue-800 active:bg-white active:border-blue-800 active:text-blue-800 text-white text-center">Contact Us</a>
        </div>
+       <div class="bars-menu active:bg-blue-800 cursor-pointer justify-center items-center w-10 h-10 rounded-[50%] bg-black text-white">
+            <i class="fas fa-bars bars-icon"></i>
+        </div>
     `;
-
   return navbar;
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import _ from "lodash";
 import Navbar from "./components/Home/navbar";
 import "./css/main.css";
 
+// Font Awesome Installing;
+const fontAwesomeAll = document.createElement("link");
+fontAwesomeAll.href =
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css";
+fontAwesomeAll.rel = "stylesheet";
+fontAwesomeAll.integrity =
+  "sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==";
+fontAwesomeAll.crossOrigin = "anonymous";
+fontAwesomeAll.referrerPolicy = "no-referrer";
+document.head.append(fontAwesomeAll);
 
-
-// intalling tailwindcss;
-const tailwindcss = document.createElement('script');
+// Tailwindcss Installing;
+const tailwindcss = document.createElement("script");
 tailwindcss.src = "https://cdn.tailwindcss.com";
 document.body.append(tailwindcss);
 
@@ -14,3 +23,14 @@ const container = document.createElement("div");
 container.className = "w-full min-h-screen";
 container.append(Navbar());
 document.body.prepend(container);
+
+// Toggle Menu
+const barsMenu = document.querySelector(".bars-menu"),
+  navMenu = document.querySelector(".nav-menu"),
+  barsIcon = document.querySelector(".bars-icon");
+barsMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("active-menu");
+  barsIcon.className === "fas fa-bars bars-icon"
+    ? (barsIcon.className = "fas fa-moon bars-icon")
+    : (barsIcon.className = "fas fa-bars bars-icon");
+});
